01245305344d8872c6a073a300843b161f56d108
host2318 <30349081+host2318@users.noreply.github.com>
2020-06-12 23:18:10 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -9,4 +9,7 @@
 #============================================================
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.8.1/g' package/base-files/files/bin/config_generate
+echo "src-git helloworld https://github.com/fw876/helloworld" >> feeds.conf.default
+./scripts/feeds update -a
+./scripts/feeds install -a
